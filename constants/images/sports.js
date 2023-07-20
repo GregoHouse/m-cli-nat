4413a4ad6ee088d128361208d2da1f02269a78d7
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-07-20 17:56:48 -0500

    feat: export sport icons images

---
+++ constants/images/sports.js
@@ -0,0 +1,51 @@
+import PadelActive from '../../assets/icons/active/padel.png';
+import PadelInactive from '../../assets/icons/inactive/padel.png';
+
+import BasquetActive from '../../assets/icons/active/basquet.png';
+import BasquetInactive from '../../assets/icons/inactive/basquet.png';
+
+import GolfActive from '../../assets/icons/active/golf.png';
+import GolfInactive from '../../assets/icons/inactive/golf.png';
+
+import TennisActive from '../../assets/icons/active/tenis.png';
+import TennisInactive from '../../assets/icons/inactive/tenis.png';
+
+import FootballActive from '../../assets/icons/active/football.png';
+import FootballInactive from '../../assets/icons/inactive/football.png';
+
+import HockeyActive from '../../assets/icons/active/hockey.png';
+import HockeyInactive from '../../assets/icons/inactive/hockey.png';
+
+import SquashActive from '../../assets/icons/active/squash.png';
+import SquashInactive from '../../assets/icons/inactive/squash.png';
+
+export const SPORT_ICONS = {
+	Padel: {
+		active: PadelActive,
+		inactive: PadelInactive,
+	},
+	Basquest: {
+		active: BasquetActive,
+		inactive: BasquetInactive,
+	},
+	Golf: {
+		active: GolfActive,
+		inactive: GolfInactive,
+	},
+	Tennis: {
+		active: TennisActive,
+		inactive: TennisInactive,
+	},
+	Football: {
+		active: FootballActive,
+		inactive: FootballInactive,
+	},
+	Hockey: {
+		active: HockeyActive,
+		inactive: HockeyInactive,
+	},
+	Squash: {
+		active: SquashActive,
+		inactive: SquashInactive,
+	},
+};
